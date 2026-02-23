4c6259b6246a86fa27bfc4fe3fb2d7fa8feb32ef
eireirjp <eireirjp@gmail.com>
2026-02-23 12:09:29 +0900

    setup pages

--- vite.config.js
+++ vite.config.js
@@ -2,27 +2,29 @@ import { resolve } from 'path'
 
 export default {
   root: resolve(__dirname, 'src'),
+  base: process.env.NODE_ENV === 'production' ? '/mico2/' : '/',
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    emptyOutDir: true
   },
   server: {
     port: 8080
   },
   // Optional: Silence Sass deprecation warnings. See note below.
   css: {
-     preprocessorOptions: {
-        scss: {
-          silenceDeprecations: [
-            'import',
-            'mixed-decls',
-            'color-functions',
-            'global-builtin',
-          ],
-          // 依存関係（node_modules）からの警告を非表示にする
-          quietDeps: true,
-          // 必要に応じて、新しいJS APIを使用する指定（Sassのバージョンによる）
-          api: 'modern-compiler'
-        },
-     },
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: [
+          'import',
+          'mixed-decls',
+          'color-functions',
+          'global-builtin',
+        ],
+        // 依存関係（node_modules）からの警告を非表示にする
+        quietDeps: true,
+        // 必要に応じて、新しいJS APIを使用する指定（Sassのバージョンによる）
+        api: 'modern-compiler'
+      },
+    },
   },
 }
